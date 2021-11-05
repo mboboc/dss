5e39bb4bfcaa1ae439f68f7311b053656f4f36c9
mboboc <madaeboboc@gmail.com>
2021-11-06 01:42:43 +0200

    Lab3 - Partial

---
+++ 3-todo-list/src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in todoList" :key="item.id">
+        <TodoItem
+            :description="item.description"
+            v-on:delete-item="deleteItem"
+            v-on:edit-item="editItem"
+        />
+        <br>
+      </li>
+      <h5>Add new todo item</h5>
+      <input v-model="newitem" placeholder="Add description">
+      <button v-on:click="addToList">Save</button>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem.vue";
+export default {
+  name: "Home",
+  components: {
+    TodoItem,
+  },
+  data() {
+    const todoList = [
+      { id: 0, description: "Get A" },
+      { id: 1, description: "Get B" },
+      { id: 2, description: "Get C" },
+    ];
+    const newitem = "";
+    return {
+        todoList,
+        newitem,
+    }
+  },
+  methods: {
+    addToList: function () {
+        this.todoList.push({ id: this.todoList.length, description: this.newitem })
+    },
+    deleteItem: function () {
+        console.log("Delete")
+    },
+    editItem: function () {
+        console.log("Edit")
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ 3-todo-list/src/components/TodoItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="todo-box">
+      <span v-if="editable">
+        <input v-model="description">
+        <button v-on:click="viewMode">Save</button>
+      </span>
+      <span v-else>
+        <p>{{ description }}</p>
+        <button> Delete </button>
+        <button v-on:click="editMode">Edit</button>
+      </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: {
+    description: String
+  },
+  data () {
+    const editable = true;
+    return {
+        editable,
+    }
+  },
+  methods: {
+    editMode: function () {
+        this.editable = true
+    },
+    viewMode: function() {
+        this.editable = false
+    },
+    deleteItem: function() {
+        this.$emit('delete-item')
+    },
+    saveItem: function() {
+        this.$emit('edit-item')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.todo-box {
+  width: 320px;
+  padding: 10px;
+  border: 5px solid gray;
+  margin: 0;
+}
+</style>
